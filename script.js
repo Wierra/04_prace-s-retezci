3f751d77f7fc8670dc274fb8dc38d0f26c67a910
Věra Juračáková <juracakova.vera@gmail.com>
2024-10-14 21:50:18 +0200

    Uložení rozpracované verze

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Vlastnosti a metody
+document.body.innerHTML = "<p><h3>Cvičení: Vlastnosti a metody</h3></p>"
+
 const title = "Všechno nebo nic"
 
 console.log(title.length)
@@ -7,20 +9,34 @@ console.log(title.toUpperCase())
 console.log(title.slice(0, 5))
 console.log(title.slice(11, 16))
 
+document.body.innerHTML += `<p>Počet znaků: ${title.length}`
+document.body.innerHTML += `<p>Název filmu velkými písmeny: ${title.toUpperCase()}</p>`
+document.body.innerHTML += `<p>Prvních 5 znaků: ${title.slice(0, 5)}</p>`
+
+const pozicePoslednihoZnaku = title.length -1
+const pozicePatehoZnakuOdKonce = title.length -5
+
+document.body.innerHTML += `<p>Posledních 5 znaků: ${title.slice(pozicePatehoZnakuOdKonce, pozicePoslednihoZnaku + 1)}</p>`
+
+
 
 //E-maily
+document.body.innerHTML += "<p><h3>Cvičení: E-maily</h3></p>"
 
-const emailUser = Number(prompt("Zadej svou e-mailovou adresu"))
+const emailUser = prompt("Zadej svou e-mailovou adresu")
 
-const atIndex = console.log(emailUser.indexOf("@"))     //pozice @
+const atIndex = emailUser.indexOf("@")     //pozice @
 
-console.log(emailUser.slice("jmeno"))
-console.log(domena.slice("domena.cz"))
-
+const userName = emailUser.slice(0, atIndex)
+const domain = emailUser.slice(atIndex, +1)
 
 const parsedEmail = {
-    userName: 'slavomir.ponuchalek',
-    domain: 'yahoo.com',
+    userName,
+    domain,
   }
 
-document.body.innerHTML += "<p>" + parsedEmail(userName) + "@" + parsedEmail(doman) + "</p>"
+document.body.innerHTML += `<p>Uživatelské jméno: ${parsedEmail.userName}</p>
+                            <p>Doména: ${parsedEmail.domain}</p>`
+                            
+document.body.innerHTML += `<p>Uživatelské jméno: ${parsedEmail.userName}</p>
+                            <p>Doména: ${parsedEmail.domain}</p>`
