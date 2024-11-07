4235610bc46d6317275c3795f6926e361ece88f6
Věra Juračáková <juracakova.vera@gmail.com>
2024-11-07 19:18:43 +0100

    Řešení Michal

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ const pozicePatehoZnakuOdKonce = title.length -5
 
 document.body.innerHTML += `<p>Posledních 5 znaků: ${title.slice(pozicePatehoZnakuOdKonce, pozicePoslednihoZnaku + 1)}</p>`
 
+//---řešení Michal
+//const title = "Lord of the Rings";
+//document.body.innerHTML += title.length;
+//document.body.innerHTML += `<br>`;
+//document.body.innerHTML += title.toUpperCase();
+//document.body.innerHTML += `<br>`;
+//document.body.innerHTML += title.slice(0, 5);
+//document.body.innerHTML += `<br>`;
+//document.body.innerHTML += title.slice(title.length - 5, title.length)
+
 
 
 //E-maily
@@ -40,3 +50,14 @@ document.body.innerHTML += `<p>Uživatelské jméno: ${parsedEmail.userName}</p>
                             
 document.body.innerHTML += `<p>Uživatelské jméno: ${parsedEmail.userName}</p>
                             <p>Doména: ${parsedEmail.domain}</p>`
+
+//--řešení Michal
+//const email = prompt('Zadejte e-mail:');
+//const atIndex = email.indexOf('@');
+//const parsedEmail = {
+//  userName: email.slice(0, atIndex),
+//  domain: email.slice(atIndex + 1),
+//};
+//document.body.innerHTML =
+//  '<p>Uživatelské jméno: ' + parsedEmail.userName + '</p>';
+//document.body.innerHTML += '<p>Doména: ' + parsedEmail.domain + '</p>';
